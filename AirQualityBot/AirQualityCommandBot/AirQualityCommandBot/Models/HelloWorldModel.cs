2eba3aa4c6f7c0e89e3d2251dc49b215c1dd5aa7
James Montemagno <jamont@microsoft.com>
2022-09-29 15:10:30 -0700

    Scaffold Apps

---
+++ AirQualityBot/AirQualityCommandBot/AirQualityCommandBot/Models/HelloWorldModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AirQualityCommandBot.Models
+{
+    public class HelloWorldModel
+    {
+        public string Title { get; set; }
+        
+        public string Body { get; set; }
+    }
+}
